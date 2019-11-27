2e015a3c728e197ee54c39c9ad0dccd74e1955a1
Martin Hudik <hudikm@gmail.com>
2019-11-27 12:33:41 +0100

    Initial commit

---
+++ source/MKL25Z128xxx4_Project.cpp
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2016-2019 NXP
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * o Redistributions of source code must retain the above copyright notice, this list
+ *   of conditions and the following disclaimer.
+ *
+ * o Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ *
+ * o Neither the name of NXP Semiconductor, Inc. nor the names of its
+ *   contributors may be used to endorse or promote products derived from this
+ *   software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+ 
+/**
+ * @file    MKL25Z128xxx4_Project.cpp
+ * @brief   Application entry point.
+ */
+
+#include <board.h>
+#include <clock_config.h>
+#include <fsl_debug_console.h>
+#include <fsl_pit.h>
+#include <peripherals.h>
+#include <pin_mux.h>
+
+
+extern "C" void PIT_IRQHandler(){
+	GPIO_TogglePinsOutput(BOARD_INITPINS_LED_RED_GPIO,1U << BOARD_INITPINS_LED_RED_PIN);
+	PIT_ClearStatusFlags(PIT, kPIT_Chnl_0, kPIT_TimerFlag);
+}
+
+int main(void) {
+	/* Structure of initialize PIT */
+    pit_config_t pitConfig;
+
+	/*
+	 * pitConfig.enableRunInDebug = false;
+	 */
+	PIT_GetDefaultConfig(&pitConfig);
+
+	/* Init pit module */
+	PIT_Init(PIT, &pitConfig);
+
+	/* Set timer period for channel 0 */
+	PIT_SetTimerPeriod(PIT, kPIT_Chnl_0, USEC_TO_COUNT(1000000U, CLOCK_GetBusClkFreq()));
+
+	/* Enable timer interrupts for channel 0 */
+	PIT_EnableInterrupts(PIT, kPIT_Chnl_0, kPIT_TimerInterruptEnable);
+
+	/* Enable at the NVIC */
+	EnableIRQ(PIT_IRQn);
+
+	/* Start channel 0 */
+	PRINTF("\r\nStarting channel No.0 ...");
+	PIT_StartTimer(PIT, kPIT_Chnl_0);
+
+  	/* Init board hardware. */
+    BOARD_InitBootPins();
+    BOARD_InitBootClocks();
+    BOARD_InitBootPeripherals();
+  	/* Init FSL debug console. */
+    BOARD_InitDebugConsole();
+
+    PRINTF("Hello World\n");
+
+    /* Force the counter to be placed into memory. */
+    volatile static int i = 0 ;
+    /* Enter an infinite loop, just incrementing a counter. */
+    while(1) {
+        i++ ;
+        /* 'Dummy' NOP to allow source level single stepping of
+            tight while() loop */
+        __asm volatile ("nop");
+    }
+    return 0 ;
+}
